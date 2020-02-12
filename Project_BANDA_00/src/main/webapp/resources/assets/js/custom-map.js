1479d78b79c35650ad4d44a1168c3a6ebeb77c3f
WishZin <heejikh@gmail.com>
2020-02-12 14:31:50 +0900

    주희진_200212_v2
    @ mapHospital.jsp  - CodeLineReset All / AddTestCode
    @ BandaController.java  - AddTestCode
    @ custom-map.js  - AddTestCode

--- Project_BANDA_00/src/main/webapp/resources/assets/js/custom-map.js
+++ Project_BANDA_00/src/main/webapp/resources/assets/js/custom-map.js
@@ -2,11 +2,40 @@
 $(function(){
 	
 	/* Search Input */
-	
-	
-	
-	
+	$("#searchInput").autocomplete({
+		source: function(request, response){
+			$.ajax({
+				type:"post",
+				url:"autocompleteAjax.do",
+				data:{value: request.term},
+				dataType:"json",
+				success: function(result){
+					
+					/* Test Code*/
+					if(result.success==true){
+						list = result.list;
+						
+						$.each(list, function(key, value){
+							console.log("key: " + key+ "   value: " + value);
+						})
+					}
+					
+				},
+				error: function(){
+					alert("ajax 서버와 통신 실패");
+				}
+				
+				
+			})//ajax end
+		},//source end
+		minLength:1,
+		select:function(event, ui){
+		
+		}
+		
+		
+	});
 	/* Map */
 	
-})
+});//function
 
